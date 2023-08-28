a00e2ae6288d7a686f3b92d2823ba09b5b41a8dd
Komail Kanjee <1796298+KomailKanjee@users.noreply.github.com>
2023-08-28 19:48:28 -0400

    migrate metaz route to flask-restx

--- app/routes/metaz.py
+++ app/routes/metaz.py
@@ -1,21 +1,24 @@
 import os
 
 from flask import Blueprint, url_for
+from flask_restx import Api, Resource
 
 blueprint = Blueprint(__name__.replace(".", "_"), __name__)
+api = Api(blueprint)
 
 
-@blueprint.route("")
-@blueprint.route("/")
-def index():
-    meta = {
-        "version": os.environ["PROXY_GATE_VERSION"],
-        "app_name": "Proxy Gate",
-        "google_auth": {
-            "session_endpoint": url_for("app_routes_auth_google.get_session"),
-        },
-        "plex_auth": {
-            "session_endpoint": url_for("app_routes_auth_plex.get_session"),
-        },
-    }
-    return meta, 200
+@api.route("")
+class Metaz(Resource):
+    def get(self):
+        meta = {
+            "version": os.environ["PROXY_GATE_VERSION"],
+            "app_name": "Proxy Gate",
+            "google_auth": {
+                "client_id": "",
+                "session_endpoint": url_for("app_routes_auth_google.get_session"),
+            },
+            "plex_auth": {
+                "session_endpoint": url_for("app_routes_auth_plex.get_session"),
+            },
+        }
+        return meta, 200
