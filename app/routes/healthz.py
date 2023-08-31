6be7326bee03710ddcfccb35bf804ddf3bcca09e
Komail Kanjee <1796298+KomailKanjee@users.noreply.github.com>
2023-08-31 01:22:23 -0400

    migrate healthz to flask-restx

--- app/routes/healthz.py
+++ app/routes/healthz.py
@@ -1,11 +1,24 @@
-from flask_restx import Namespace, Resource
+from flask_restx import Namespace, Resource, fields
 
-api = Namespace(
-    "Healthz", description="Application health realated endpoints"
-)
+ns = Namespace("Healthz", description="Application health realated endpoints")
 
 
-@api.route("")
+class Models:
+    Healthz = ns.model(
+        "Healthz",
+        {
+            "status": fields.String(
+                example="healthy",
+                description="Overall application health status",
+                enum=["healthy", "error"],
+            ),
+        },
+        strict=True,
+    )
+
+
+@ns.route("")
 class Healthz(Resource):
+    @ns.marshal_with(Models.Healthz, description="Application health status")
     def get(self):
-        return "OK", 200
+        return {"status": "healthy"}, 200
